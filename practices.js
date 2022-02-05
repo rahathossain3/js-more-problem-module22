d00078e8e44306ae56d3798a2b9d5405c442e87a
rahathossain3 <rahathossain210@gmail.com>
2022-02-05 23:03:26 +0600

    practice added

--- practices.js
+++ practices.js
@@ -40,20 +40,20 @@ console.log(arr, arrLength);
 // -------------------------------------------------------------------
 
 // ৯. তুমি কি একটা ফর লুপ দিয়ে কোন একটা array এর সবগুলা উপাদানকে দেখাতে পারবে। সেটা রেগুলার for লুপ হোক বা for of হোক। হলে সেই স্টাইলে একটা কোড লিখে ফেলো।
-/*
+
 var arr = [4, 8, 7, 96, 78, 5, 5, 5, 45, 6, 5];
 console.log(arr.length);
 
 for (var i = 0; i < arr.length; i++) {
-    const newArr = arr[i];
-    // console.log(newArr);
+    // const newArr = arr[i];               //not needed
+    console.log(arr[i]);
 }
- */
+
 
 //-------------------------------------------------------
 // ১০. তোমাকে যদি বলা হয়। একটা array এর মধ্যে ৮০ এর চাইতে বড় সংখ্যা থাকলে সেগুলাকে console log করে দেখাতে সেটা কি তুমি পারবে? তাহলে তুমি সেই কোড করে ফেলো
 
-/*
+/* 
 function largestNumber(numbers) {
     let largest = numbers[0];
     for (let i = 0; i < numbers.length; i++) {
@@ -61,23 +61,21 @@ function largestNumber(numbers) {
         if (rahat > 80) {
             largest = rahat;
             console.log(rahat)
-
         }
-
     }
-    // return largest;
+    // return largest;              //not needed
 }
 var urmi = [56, 87, 89, 52, 2, 5, 23, 6, 54, 95, 1];
 const love = largestNumber(urmi);
 // console.log(love);
+
  */
 
 
 
-
 // ১১. তিনটা সংখ্যার গুনফল বের করে ফাইনাল রেজাল্ট আউটপুট হিসেবে রিটার্ন করতে হবে। তুমি কি সেই কোড লিখতে পারবে। যদি পারো তাহলে সেই কোড লিখে ফেলো।
+/* 
 
-/*
 function multiplay(num1, num2, num3) {
     const result = num1 * num2 * num3;
     return result;
@@ -85,18 +83,18 @@ function multiplay(num1, num2, num3) {
 const finalNumber = multiplay(15, 85, 8);
 console.log(finalNumber);
 
-
  */
+
 
 
 // ১২. একটা অবজেক্ট ডিক্লেয়ার করবে। সেটাতে তিনটা প্রপার্টি থাকবে। এবং কোন একটা প্রোপার্টি এর মান চেইঞ্জ করবা।
 
 /* let sumaiya = { color: 'red', age: 20, home: "barishal" };
 sumaiya.home = 'dhaka';
-console.log(sumaiya); */
+console.log(sumaiya);  */
 
 //১. সিম্পল একটা ফাংশন লিখতে হবে। যেটার নাম হবে feetToInch এবং এই ফাংশন ইনপুট হিসেবে নিবে feet আর রিটার্ন করবে inch । অর্থাৎ এই ফাংশনকে কোন একটা ফিট বলে দিলে সে রিটার্ন হিসেবে বলে দিবে কত ইঞ্চি হয়।
-/*
+/* 
 function feetToInch(feet) {
     let inch = feet * 12;
     return inch;
@@ -118,9 +116,9 @@ console.log(inch, 'inch');
 const centimeterIs = 500;
 const meter = centimeterToMeter(centimeterIs);
 console.log(meter, 'meter');
-
-
  */
+
+
 
 
 // -----------------------------------------------------------
@@ -137,7 +135,7 @@ console.log(meter, 'meter');
 
 
 
-/*
+/* 
 function paperRequirements(firstBook, secondBook, thirdBook) {
     const firstParBookPaper = 100;
     const secondParBookPaper = 200;
@@ -155,34 +153,65 @@ function paperRequirements(firstBook, secondBook, thirdBook) {
 }
 
 const totalBookPaper = paperRequirements(1, 5, 10);
-console.log(totalBookPaper);
+console.log(totalBookPaper); */
 
 
- */
 
-// --------------------------------------------
 
+// -----------------------------------------------
+
+// ৫. এইটা একটু ট্রিকি হতে পারে। একটা array এর মধ্যে অনেকগুলা সংখ্যা থাকবে। তোমার কাজ হচ্ছে সংখ্যা গুলা একটার পর একটা করে চেক করা। এবং সংখ্যা গুলা যদি পজিটিভ সংখ্যা হয়। অর্থাৎ শূন্য বা শূন্যের চাইতে বড় হয় তাহলে সেগুলাকে কোন একটা array এর মধ্যে রাখবে। আর যদি নেগেটিভ সংখ্যা হয়। তাহলে লুপটা স্টপ করে দিবে। এবং লুপ বন্ধ করার আগ পর্যন্ত যতগুলা পজিটিভ সংখ্যা পাওয়া গেছে। সেগুলা রিটার্ন করে দিবে।
+
+/* function positiveNumber(numbers) {
+    let positiveArray = [];
+    for (const number of numbers) {
+        if (number >= 0) {
+            positiveArray.push(number);
+        }
+        else {
+            return positiveArray;
+        }
+    }
+    return positiveArray;
+}
+const numbersArray = [51, 89, 69, 15, -4, 84, 2];
+const positiveNumberIs = positiveNumber(numbersArray);
+console.log(positiveNumberIs); */
+
+
+
+
+
+
+
+
+// -----------------------------------------
 
 /* 
 ৪. একটা ফাংশন লিখবে। এই ফাংশনের নাম হবে bestFriend তারপর সেই ফাংশনে ইনপুট প্যারামিটার হিসেবে একটা array নিবে। সেই array এর মধ্যে তোমার সব ফ্রেন্ডের নাম থাকবে। এখন তোমার কাজ হচ্ছে যে ফ্রেন্ড এর নাম সবচেয়ে বড় সেই ফ্রেন্ড এর নাম রিটার্ন করে দেয়া। এই ক্ষেত্রে তুমি নামটা অর্থাৎ ফ্রেন্ডের নাম (স্ট্রিং) রিটার্ন করতে হবে।  */
 
+
 function bestFriend(name) {
     let largeFriendName = name[0];
+
     for (var i = 0; i < name.length; i++) {
-        var largest = name[i];
-        console.log(i, largest);
-        if (name.length[i] > largest.length) {
-            console.log('ok');
+        var largest = name[i];                  //
+        // console.log(i, largest);
+
+        if (largeFriendName.length < largest.length) { // chack by defult data
+            largeFriendName = largest;
+            // console.log('ok');
         }
+
     }
-
-
+    return largeFriendName;
 
 }
 
-const bestFriendName = ['rahat', 'fardush', 'munna', 'rakib', 'shakil', 'rana'];
+const bestFriendName = ['rahat', 'fardush', 'munna', 'rakib', 'shakil', 'rana',];
 const friendLargeName = bestFriend(bestFriendName);
-// console.log(friendLargeName);
+
+console.log(friendLargeName);
 
 
 
